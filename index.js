01f3b1598f45beeb1c2597ef78d0127679e1570d
Erwann LAGOUCHE <erwann.lagouche@lamache.lan>
2021-05-25 17:48:56 +0200

    😖 Updated dashboard

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ db = mysql.createConnection({
 });
 
 // Initialization
-require('./lib/init')(db);      // Initialization task
+require('./lib/init');      // Initialization task
 require('./lib/periodic')       // Periodic tasks
 require('./dataAPI');           // REST API
 
@@ -39,51 +39,56 @@ const port = process.env.PORT || 80; // Web server port
 webServer.use(cookieParser());
 webServer.use(jsonParser);
 webServer.use((req, res) => {
-    // Redirecting
-    if (req.originalUrl === '/' && Object.keys(req.cookies).length === 0) {
-        res.send(`<html lang="en"><body><script>window.location.href="http://${req.hostname}/login"</script></body></html>`);
-    } else if (req.originalUrl === '/password') {
-        if (checkPassword(req.body["password"])) {
-            const date = new Date(Date.now());
-            dbg(`${req.ip} connected at ${Date.now()} (${date.getMonth() + 1}/${date.getDay()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}''${date.getSeconds()})`);
-            // ^ Adding one to the month because month convention is -1 by default (starting with zero)
+        // Redirecting
+        if (req.originalUrl === '/') {
+            if (Object.keys(req.cookies).length === 0) {
+                res.send(`<html lang="en"><body><script>window.location.href="http://${req.hostname}/login"</script></body></html>`);
+            }
+            if (Object.keys(req.cookies).length !== 0) {
+                // Bad cookies handler
+                if (!req.cookies.hasOwnProperty('key')) {
+                    res.send('<html><h1>Erreur 401 (oula!)</h1><br/>Vos cookies sont invalides.<br/>Veuillez les supprimer, vider le cache du navigateur ou relancer cette page en navigation privée.</html>')
+                } else {
+                    require('./lib/form')(db, req.cookies['key'], res2 => {
+                        res.send(res2);
+                    })
+                    //res.send(`<html>Connecté avec la clé "<em>${req.cookies['key']}</em>"<br/>Cookies: "<em>${JSON.stringify(req.cookies)}</em>"</html>`)
+                }
+            }
+        } else if (req.originalUrl === '/password') {
+            if (checkPassword(req.body["password"])) {
+                const date = new Date(Date.now());
+                dbg(`${req.ip} connected at ${Date.now()} (${date.getMonth() + 1}/${date.getDay()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}''${date.getSeconds()})`);
+                // ^ Adding one to the month because month convention is -1 by default (starting with zero)
 
-            const id = Math.floor(Math.random() * 65535);   // Creates random id
+                const id = Math.floor(Math.random() * 65535);   // Creates random id
 
-            // Adds instance to db
-            newInstance(req.ip, id)
-            // Sends response
-            res.setHeader('Content-Type', 'application/json');
-            res.send(`{"key":"${id}"}`);
-            dbg(`Successfully pushed instances for ${req.ip} to database`);
+                // Adds instance to db
+                newInstance(req.ip, id)
+                // Sends response
+                res.setHeader('Content-Type', 'application/json');
+                res.send(`{"key":"${id}"}`);
+                dbg(`Successfully pushed instances for ${req.ip} to database`);
+            } else {
+                res.sendStatus(401);
+                const date = new Date(Date.now());
+                dbg(`${req.ip} tried a wrong password at ${Date.now()} (${date.getMonth() + 1}/${date.getDay()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}''${date.getSeconds()})`);
+            }
         } else {
-            res.sendStatus(401);
-            const date = new Date(Date.now());
-            dbg(`${req.ip} tried a wrong password at ${Date.now()} (${date.getMonth() + 1}/${date.getDay()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}''${date.getSeconds()})`);
-        }
-    } else if (Object.keys(req.cookies).length !== 0) {
-        // Bad cookies handler
-        if (!req.cookies.hasOwnProperty('key')) {
-            res.send('<html><h1>Erreur 401 (oula!)</h1><br/>Vos cookies sont invalides.<br/>Veuillez les supprimer, vider le cache du navigateur ou relancer cette page en navigation privée.</html>')
-        } else {
-            // TODO: Actually handle good connections here
-            res.sendFile(path.resolve(__dirname, './web/dashboard.html'));
-            //res.send(`<html>Connecté avec la clé "<em>${req.cookies['key']}</em>"<br/>Cookies: "<em>${JSON.stringify(req.cookies)}</em>"</html>`)
-        }
-    } else {
-        // Sending the appropriate file
-        let file = req.originalUrl.replace('/', '').split('?')[0]   // Removes the '/' for the file search, as well as options
-        if (!file.includes('.')) file = file + '.html'  // Adds an extension for the file search
-        file = './web/' + file;
-        const filePath = path.resolve(__dirname, file);
-        dbg(filePath);  // Some verbose
-        if (fs.existsSync(filePath)) {
-            res.sendFile(filePath); // Then gets the file and sends it
-        } else {
-            res.sendStatus(404);    // Sends 404
+            // Sending the appropriate file
+            let file = req.originalUrl.replace('/', '').split('?')[0]   // Removes the '/' for the file search, as well as options
+            if (!file.includes('.')) file = file + '.html'  // Adds an extension for the file search
+            file = './web/' + file;
+            const filePath = path.resolve(__dirname, file);
+            dbg(filePath);  // Some verbose
+            if (fs.existsSync(filePath)) {
+                res.sendFile(filePath); // Then gets the file and sends it
+            } else {
+                res.sendStatus(404);    // Sends 404
+            }
         }
     }
-})
+)
 dbg(`Server listening on port ${port}, '/'`);
 
 // Server starts
